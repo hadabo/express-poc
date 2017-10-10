e0667144301857476a2cfcb14e2082da0a3b6a9e
Abdul Hadi Hawari <ah.hawari@gmail.com>
2017-10-10 15:22:38 +0300

    add post, put & delete routes / test params and next

--- index.js
+++ index.js
@@ -2,13 +2,51 @@ const express = require('express')
 const chalk = require('chalk')
 
 const app = express()
+const log = console.log
 
-app.get('/', function(req,res){
-  res.send('Hello world!')
+// HTTP Get Method routes
+app.get('/', function(req,res,next){
+  log(chalk.yellow('get home page /'))
+  next()
+}, function(req,res){
+  res.json({res:'Hello world!'})
 })
 
 app.get('/about', function(req,res){
   res.send('This is a test for ExpressJs!')
+})
+
+// HTTP Get Method route with parameters
+app.get('/user/:id/:name', function(req,res){
+  res.json({id:req.params.id,name:req.params.name})
+})
+
+
+// HTTP Post Method routes
+app.post('/', function(req,res){
+  res.send('Post Hello world!')
+})
+
+app.post('/about', function(req,res){
+  res.send('Post This is a test for ExpressJs!')
+})
+
+// HTTP Put Method routes
+app.put('/', function(req,res){
+  res.send('Put Hello world!')
+})
+
+app.put('/about', function(req,res){
+  res.send('Put This is a test for ExpressJs!')
+})
+
+// HTTP Delete Method routes
+app.delete('/', function(req,res){
+  res.send('Delete Hello world!')
+})
+
+app.delete('/about', function(req,res){
+  res.send('Delete This is a test for ExpressJs!')
 })
 
 app.listen(3010, function(){
