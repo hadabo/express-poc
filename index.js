e994c40e3115de7cffd73696c99236b7f0e5f040
Abdul Hadi Hawari <ah.hawari@gmail.com>
2017-10-10 16:21:04 +0300

    add xo to the project

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-const express = require('express')
-const chalk = require('chalk')
+const express = require('express');
+const chalk = require('chalk');
 
-const app = express()
-const log = console.log
+const app = express();
+const log = console.log;
 
 // HTTP Get Method routes
-app.get('/', function(req,res,next){
-  log(chalk.yellow('get home page /'))
-  next()
-}, function(req,res){
-  res.json({res:'Hello world!'})
-})
+app.get('/', (req, res, next) => {
+	log(chalk.yellow('get home page /'));
+	next();
+}, (req, res) => {
+	res.json({res: 'Hello world!'});
+});
 
-app.get('/about', function(req,res){
-  res.send('This is a test for ExpressJs!')
-})
+app.get('/about', (req, res) => {
+	res.send('This is a test for ExpressJs!');
+});
 
 // HTTP Get Method route with parameters
-app.get('/user/:id/:name', function(req,res){
-  res.json({id:req.params.id,name:req.params.name})
-})
-
+app.get('/user/:id/:name', (req, res) => {
+	res.json({id: req.params.id, name: req.params.name});
+});
 
 // HTTP Post Method routes
-app.post('/', function(req,res){
-  res.send('Post Hello world!')
-})
+app.post('/', (req, res) => {
+	res.send('Post Hello world!');
+});
 
-app.post('/about', function(req,res){
-  res.send('Post This is a test for ExpressJs!')
-})
+app.post('/about', (req, res) => {
+	res.send('Post This is a test for ExpressJs!');
+});
 
 // HTTP Put Method routes
-app.put('/', function(req,res){
-  res.send('Put Hello world!')
-})
+app.put('/', (req, res) => {
+	res.send('Put Hello world!');
+});
 
-app.put('/about', function(req,res){
-  res.send('Put This is a test for ExpressJs!')
-})
+app.put('/about', (req, res) => {
+	res.send('Put This is a test for ExpressJs!');
+});
 
 // HTTP Delete Method routes
-app.delete('/', function(req,res){
-  res.send('Delete Hello world!')
-})
+app.delete('/', (req, res) => {
+	res.send('Delete Hello world!');
+});
 
-app.delete('/about', function(req,res){
-  res.send('Delete This is a test for ExpressJs!')
-})
+app.delete('/about', (req, res) => {
+	res.send('Delete This is a test for ExpressJs!');
+});
 
-app.listen(3010, function(){
-  console.log('Example app listengin on port 3010! ('+ chalk.blue('http://localhost:3010') + ')')
-})
+app.listen(3010, () => {
+	console.log('Example app listengin on port 3010! (' + chalk.blue('http://localhost:3010') + ')');
+});
