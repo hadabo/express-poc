c9c51d05bd73f70ca635a3ca94836a1145e2dd68
Abdul Hadi Hawari <ah.hawari@gmail.com>
2017-10-10 14:42:15 +0300

    add chalk and about rooute

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const chalk = require('chalk')
+
 const app = express()
 
 app.get('/', function(req,res){
   res.send('Hello world!')
 })
 
+app.get('/about', function(req,res){
+  res.send('This is a test for ExpressJs!')
+})
+
 app.listen(3010, function(){
-  console.log('Example app listengin on port 3010!')
+  console.log('Example app listengin on port 3010! ('+ chalk.blue('http://localhost:3010') + ')')
 })
