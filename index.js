effc72a0d5a54830e17458fce9e5aaf8787f9c96
Abdul Hadi Hawari <ah.hawari@gmail.com>
2017-10-11 17:30:29 +0300

    update some messages

--- index.js
+++ index.js
@@ -9,29 +9,39 @@ const log = console.log;
 // Connection URL
 const url = 'mongodb://localhost:27017/express-mongo';
 
+const currentRequest = function (req) {
+	const fullUrl = req.protocol + '://' + req.get('Host') + req.url;
+	const url = '⛔️ [' + chalk.yellow(req.method) + '] method on [' + chalk.blue(fullUrl) + '] URL is not supported yet!';
+	return req ? url : false;
+};
+
 // HTTP Get Method routes
-app.get('/', (req, res, next) => {
-	log(chalk.yellow('[/] route is called'));
-	next();
-}, (req, res) => {
+app.get('/', (req, res) => {
 	const results = [];
 
   // Connect MongonDB to get the user-data results
 	mongo.connect(url, (err, db) => {
 		assert.equal(null, err);
+		log(chalk.green('MongoDB is connected 👍🏻'));
 		const cursor = db.collection('user-data').find();
 		cursor.forEach((doc, err) => {
 			assert.equal(null, err);
 			results.push(doc);
 		}, () => {
 			db.close();
+			if (results.length > 0) {
+				log(chalk.green(results.length + ' Users found'));
+			} else {
+				log(chalk.red('Sorry, no users found!'));
+			}
 			res.json(results);
 		});
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.send('This is a test for ExpressJs!');
+	log(currentRequest(req));
+	res.send(currentRequest(req));
 });
 
 // HTTP Get Method route with parameters
@@ -46,12 +56,20 @@ app.get('/user/:id/:name', (req, res) => {
   // Connect MongonDB to get the user-data results
 	mongo.connect(url, (err, db) => {
 		assert.equal(null, err);
+		log(chalk.green('MongoDB is connected 👍🏻'));
 		const cursor = db.collection('user-data').find(user);
 		cursor.forEach((doc, err) => {
 			assert.equal(null, err);
 			results.push(doc);
 		}, () => {
 			db.close();
+
+			if (results.length > 0) {
+				log(chalk.green('User found\n'));
+			} else {
+				log(chalk.red('User not found'), user);
+			}
+
 			res.json(results);
 		});
 	});
@@ -59,11 +77,13 @@ app.get('/user/:id/:name', (req, res) => {
 
 // HTTP Post Method routes
 app.post('/', (req, res) => {
-	res.send('Post Hello world!');
+	log(currentRequest(req));
+	res.send(currentRequest(req));
 });
 
 app.post('/about', (req, res) => {
-	res.send('Post This is a test for ExpressJs!');
+	log(currentRequest(req));
+	res.send(currentRequest(req));
 });
 
 // HTTP Post Method route with parameters
@@ -76,10 +96,10 @@ app.post('/user/:id/:name', (req, res) => {
   // Use connect method to connect to the server
 	mongo.connect(url, (err, db) => {
 		assert.equal(null, err);
-		log(chalk.green('MongoDB connected 👍🏻'));
+		log(chalk.green('MongoDB is connected 👍🏻'));
 		db.collection('user-data').insertOne(user, (err, res) => {
 			assert.equal(null, err);
-			log(chalk.green('User inserted'), res.result);
+			log(chalk.green('New user inserted'), res.result);
 			db.close();
 		});
 	});
@@ -88,22 +108,31 @@ app.post('/user/:id/:name', (req, res) => {
 
 // HTTP Put Method routes
 app.put('/', (req, res) => {
-	res.send('Put Hello world!');
+	log(currentRequest(req));
+	res.send(currentRequest(req));
 });
 
 app.put('/about', (req, res) => {
-	res.send('Put This is a test for ExpressJs!');
+	log(currentRequest(req));
+	res.send(currentRequest(req));
 });
 
 // HTTP Delete Method routes
 app.delete('/', (req, res) => {
-	res.send('Delete Hello world!');
+	log(currentRequest(req));
+	res.send(currentRequest(req));
 });
 
 app.delete('/about', (req, res) => {
-	res.send('Delete This is a test for ExpressJs!');
+	log(currentRequest(req));
+	res.send(currentRequest(req));
+});
+
+app.get('*', (req, res) => {
+	log(currentRequest(req));
+	res.status(404).send(currentRequest(req));
 });
 
 app.listen(3010, () => {
-	log('Example app listengin on port 3010! (' + chalk.blue('http://localhost:3010') + ')');
+	log('👌  Express PoC is Ready: listengin on port 3010 [' + chalk.blue('http://localhost:3010') + ']');
 });
